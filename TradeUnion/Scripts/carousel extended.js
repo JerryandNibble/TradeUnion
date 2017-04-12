8397c853fdedd3bc41c6a0b1255f7a82ec9a7a6d
Tom <tom279185796@gmail.com>
2017-04-12 10:09:24 +0800

    调整几个简介栏宽度

--- TradeUnion/Scripts/carousel extended.js
+++ TradeUnion/Scripts/carousel extended.js
@@ -6,8 +6,8 @@
  
         $('#carousel-text').html($('#slide-content-0').html());
  
-        /*Handles the carousel thumbnails
-*/       $('[id^=carousel-selector-]').click( function(){
+        /*Handles the carousel thumbnails*/ 
+        $('[id^=carousel-selector-]').click( function(){
             var id = this.id.substr(this.id.lastIndexOf("-") + 1);
             var id = parseInt(id);
             $('#myCarousel').carousel(id);
