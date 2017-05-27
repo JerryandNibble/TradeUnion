2d07d1707ff58a40dc52124cdee1b337e2c118c4
Tom <tom279185796@gmail.com>
2017-05-27 22:02:45 +0800

    教职工福利中，实现了教职工体检、生日

--- SQLSeverDal/EmployeeBenefits/EmployeeBenefitsDal.cs
+++ SQLSeverDal/EmployeeBenefits/EmployeeBenefitsDal.cs
@@ -7,6 +7,7 @@ namespace SQLSeverDal.EmployeeBenefits
 {
     public class EmployeeBenefitsDal : BaseDal
     {
+        #region  体检活动查看的方法
         /// <summary>
         /// 体检
         /// </summary>
@@ -21,20 +22,28 @@ namespace SQLSeverDal.EmployeeBenefits
                                            ";
             return Query<TiJian>(Query_factorySql).ToList();
         }
-        /// <summary>
-        /// 添加活动的方法
-        /// </summary>
-        /// <returns></returns>
-        public IList<HuoDong> Query_HuoDong()
-        {
-            const string Query_factorySql = @"select ActMingCheng,
-                                              FaBuRen,
-                                              JieShao
-                                              from tb_huodong
-                                            ";
-            return Query<HuoDong>(Query_factorySql).ToList();
-        }
 
+        /// <summary>
+        /// 对职工体检信息遍历
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public TiJian QueryTiJian(int Id)
+        {
+            const string Query_TiJiansql = @"select bianhao,
+                                            xingming,
+                                            shijian,
+                                            yiyuan,
+                                            jieguo 
+                                            from tb_tijian
+                                            where ID = @Id
+                                           ";
+            var Model = base.QueryFirst<TiJian>(Query_TiJiansql, new { Id = Id });
+            return Model;
+        }
+        #endregion
+
+        #region 生日信息查看方法
         /// <summary>
         /// 生日
         /// </summary>
@@ -49,6 +58,26 @@ namespace SQLSeverDal.EmployeeBenefits
                                            ";
             return Query<Shengri>(Query_factorySql).ToList();
         }
+
+        /// <summary>
+        /// 对职工体检信息遍历
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public Shengri QueryShengRi(int Id)
+        {
+            const string Query_ShengRisql = @"select bianhao,
+                                            xingming,
+                                            shengri 
+                                            from tb_shengri
+                                            where ID = @Id
+                                           ";
+            var Model = base.QueryFirst<Shengri>(Query_ShengRisql, new { Id = Id });
+            return Model;
+        }
+        #endregion
+
+        #region 保险信息查看方法
         /// <summary>
         /// 保险
         /// </summary>
@@ -63,6 +92,28 @@ namespace SQLSeverDal.EmployeeBenefits
                                            ";
             return Query<Baoxian>(Query_factorySql).ToList();
         }
+
+        /// <summary>
+        /// 对职工保险信息遍历
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public Baoxian QueryBaoXian(int Id)
+        {
+            const string Query_BaoXiansql = @"select bianhao,
+                                            xingming,
+                                            youxiaoqi,
+                                            baoxian,
+                                            fenshu
+                                            from tb_baoxian
+                                            where ID = @Id
+                                           ";
+            var Model = base.QueryFirst<Baoxian>(Query_BaoXiansql, new { Id = Id });
+            return Model;
+        }
+        #endregion
+
+        #region 特殊情况查看方法
         /// <summary>
         /// 特殊情况
         /// </summary>
@@ -76,6 +127,38 @@ namespace SQLSeverDal.EmployeeBenefits
                                             from tb_teshu
                                            ";
             return Query<Teshu>(Query_factorySql).ToList();
+        }
+        /// <summary>
+        /// 遍历特殊情况返回至编辑视图
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public Teshu QueryTeShu(int Id)
+        {
+            const string Query_TeShusql = @"select bianhao,
+                                            xingming,
+                                            teshu 
+                                            from tb_teshu
+                                            where ID = @Id
+                                           ";
+            var Model = base.QueryFirst<Teshu>(Query_TeShusql, new { Id = Id });
+            return Model;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 添加活动的方法
+        /// </summary>
+        /// <returns></returns>
+        public IList<HuoDong> Query_HuoDong()
+        {
+            const string Query_factorySql = @"select ActMingCheng,
+                                              FaBuRen,
+                                              JieShao
+                                              from tb_huodong
+                                            ";
+            return Query<HuoDong>(Query_factorySql).ToList();
         }
     }
 }
--- TradeUnion/Controllers/EmployeeBenefitsController.cs
+++ TradeUnion/Controllers/EmployeeBenefitsController.cs
@@ -1,4 +1,7 @@
-﻿using SQLSeverDal.EmployeeBenefits;
+﻿using Dapper;
+using SQLServerDal;
+using SQLSeverDal.EmployeeBenefits;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Web.Mvc;
 using TU.Model.Models;
@@ -7,58 +10,17 @@ namespace TradeUnion.Controllers
 {
     public class EmployeeBenefitsController : Controller
     {
-        #region ---返回视图
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        // GET: EmployeeBenefits
+        #region 职工福利首页
         public ActionResult EmployeeBenefitsIndex()
         {
             return View();
         }
+        #endregion
+
+
+        #region 体检信息方法
         /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult AddBodyExamIndex()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 返回添加文体活动AddActivityIndex视图
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult AddActivityIndex()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult AddBirthdayIndex()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult AddInsuranceIndex()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult AddSpecialIndex()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 
+        /// 浏览体检信息的方法
         /// </summary>
         /// <returns></returns>
         public ActionResult ScanBodyExamIndex()
@@ -68,6 +30,205 @@ namespace TradeUnion.Controllers
             ViewBag.List = queryResult;
             return View();
         }
+
+        /// <summary>
+        /// 返回视图和添加体检信息方法
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddBodyExamIndex()
+        {
+            return View();
+        }
+
+        public ActionResult AddBodyExamInfor(TiJian model)
+        {
+            SQLHelper sqlh = new SQLHelper();
+            const string AddEyeBeftsql = @"INSERT INTO dbo.TB_TiJian
+                                       ( BianHao,JieGuo,ShiJian,XingMing,YiYuan)
+                                        VALUES  ( @BianHao, 
+                                                  @JieGuo,
+			                                      @ShiJian,
+			                                      @XingMing,
+                                                  @YiYuan
+            ) ";
+            SqlParameter[] para = new SqlParameter[]
+            {
+              new SqlParameter("BianHao",model.BianHao),
+              new SqlParameter("JieGuo", model.JieGuo),
+              new SqlParameter("ShiJian", model.ShiJian),
+              new SqlParameter("XingMing", model.XingMing),
+              new SqlParameter("YiYuan", model.YiYuan)
+             };
+            sqlh.ExecData(AddEyeBeftsql, para);
+            return RedirectToAction("ScanBodyExamIndex", "EmployeeBenefits");
+        }
+
+
+        public ActionResult DelBodyExamInfor(int Id = 0)
+        {
+            const string DelBodyExamsql = @"
+                        DELETE FROM dbo.tb_tijian
+                        WHERE ID = @ID
+            ";
+            using (DbConnection conn = DbFactory.CreateConnection())
+            {
+                DynamicParameters dp = new DynamicParameters();
+                dp.Add("ID", Id);
+                var result = conn.Execute(DelBodyExamsql, dp) > 0;
+            }
+            return RedirectToAction("ScanBodyExamIndex", "EmployeeBenefits");
+        }
+
+        /// <summary>
+        /// 返回Model数据和更新体检信息方法
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public ActionResult EditBodyExamInforIndex(int Id = 0)
+        {   //对应工会教职工信息的数据访问层的类，
+            //其对象通过调用QueryTiJian方法实现Model内数据取出操作并最终通过ViewData方式显示到View视图
+            EmployeeBenefitsDal bodyexaminforDal = new EmployeeBenefitsDal();
+            var queryResult = bodyexaminforDal.QueryTiJian(Id);
+            ViewData.Model = queryResult;
+            return View(ViewData.Model);
+        }
+
+        public ActionResult EditBodyExamInforSave(TiJian model)
+        {
+            const string BodyExamInforSaveSql = @"UPDATE dbo.TB_TiJian
+				                                   SET	BianHao=@BianHao,
+					                                    XingMing=@XingMing,
+					                                    ShiJian=@ShiJian,
+                                                        YiYuan=@YiYuan,
+                                                        JieGuo=@JieGuo
+				                                   WHERE ID=@ID";
+            using (DbConnection conn = DbFactory.CreateConnection())
+            {
+                var result = conn.Execute(BodyExamInforSaveSql, model) > 0;
+            }
+            return RedirectToAction("ScanBodyExamIndex", "EmployeeBenefits");
+        }
+        #endregion
+
+        #region 生日信息方法
+        public ActionResult ScanBirthdayIndex()
+        {
+            EmployeeBenefitsDal ScanBirthday = new EmployeeBenefitsDal();
+            var queryResult = ScanBirthday.Query_Shengri();
+            ViewBag.List = queryResult;
+            return View();
+        }
+
+        public ActionResult AddBirthdayIndex()
+        {
+            return View();
+        }
+
+        public ActionResult AddBriDay(Shengri model)
+        {
+            SQLHelper sqlh = new SQLHelper();
+            const string AddBriDaysql = @"INSERT INTO dbo.TB_ShengRi
+                                       ( BianHao,XingMing,ShengRi)
+                                        VALUES  ( @BianHao, 
+                                                  @XingMing,
+			                                      @ShengRi
+            ) ";
+            SqlParameter[] para = new SqlParameter[]
+            {
+              new SqlParameter("BianHao",model.BianHao),
+              new SqlParameter("XingMing", model.XingMing),
+              new SqlParameter("ShengRi", model.ShengRi)
+             };
+            sqlh.ExecData(AddBriDaysql, para);
+            return RedirectToAction("ScanBirthdayIndex", "EmployeeBenefits");
+        }
+
+        /// <summary>
+        /// 删除教职工生日信息方法
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public ActionResult DelShengRi(int Id = 0)
+        {
+            const string DelShengRisql = @"
+                        DELETE FROM dbo.tb_shengri
+                        WHERE ID = @ID
+            ";
+            using (DbConnection conn = DbFactory.CreateConnection())
+            {
+                DynamicParameters dp = new DynamicParameters();
+                dp.Add("ID", Id);
+                var result = conn.Execute(DelShengRisql, dp) > 0;
+            }
+            return RedirectToAction("ScanBirthdayIndex", "EmployeeBenefits");
+        }
+
+        public ActionResult EditShengRiIndex(int Id = 0)
+        {
+            EmployeeBenefitsDal unioninforDal = new EmployeeBenefitsDal();
+            var queryResult = unioninforDal.QueryShengRi(Id);
+            ViewData.Model = queryResult;
+            return View(ViewData.Model);
+        }
+
+        public ActionResult EditShengRiSave(JiaGou model)
+        {
+            const string EditShengRiSaveSql = @"UPDATE dbo.TB_ShengRi
+				                                   SET	BianHao=@BianHao,
+					                                    XingMing=@XingMing,
+					                                    ShengRi=@ShengRi
+				                                   WHERE ID=@ID";
+            using (DbConnection conn = DbFactory.CreateConnection())
+            {
+                var result = conn.Execute(EditShengRiSaveSql, model) > 0;
+            }
+            return RedirectToAction("ScanBirthdayIndex", "EmployeeBenefits");
+        }
+        #endregion
+
+
+        #region 保险基金方法
+        public ActionResult AddInsuranceIndex()
+        {
+            return View();
+        }
+
+
+
+        #endregion
+
+
+        #region 特殊情况方法
+        public ActionResult AddSpecialIndex()
+        {
+            return View();
+        }
+
+
+
+        #endregion
+
+        #region ---返回视图
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        // GET: EmployeeBenefits
+
+
+        /// <summary>
+        /// 返回添加文体活动AddActivityIndex视图
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddActivityIndex()
+        {
+            return View();
+        }
+
+
+
+
+
         /// <summary>
         /// 浏览文体活动方法返回视图
         /// </summary>
@@ -83,13 +244,7 @@ namespace TradeUnion.Controllers
         /// 
         /// </summary>
         /// <returns></returns>
-        public ActionResult ScanBirthdayIndex()
-        {
-            EmployeeBenefitsDal ScanBirthday = new EmployeeBenefitsDal();
-            var queryResult = ScanBirthday.Query_Shengri();
-            ViewBag.List = queryResult;
-            return View();
-        }
+        
         /// <summary>
         /// 
         /// </summary>
@@ -115,28 +270,7 @@ namespace TradeUnion.Controllers
         #endregion
 
         #region --体检信息 
-        public ActionResult AddEyeBeft(TiJian model)
-        {
-            SQLHelper sqlh = new SQLHelper();
-            const string AddEyeBeftsql = @"INSERT INTO dbo.TB_TiJian
-                                       ( BianHao,JieGuo,ShiJian,XingMing,YiYuan)
-                                        VALUES  ( @BianHao, 
-                                                  @JieGuo,
-			                                      @ShiJian,
-			                                      @XingMing,
-                                                  @YiYuan
-            ) ";
-            SqlParameter[] para = new SqlParameter[]
-            {
-              new SqlParameter("BianHao",model.BianHao),
-              new SqlParameter("JieGuo", model.JieGuo),
-              new SqlParameter("ShiJian", model.ShiJian),
-              new SqlParameter("XingMing", model.XingMing),
-              new SqlParameter("YiYuan", model.YiYuan)
-             };
-            sqlh.ExecData(AddEyeBeftsql, para);
-            return RedirectToAction("ScanBodyExamIndex", "EmployeeBenefits");
-        }
+        
 
         #endregion
 
@@ -165,24 +299,7 @@ namespace TradeUnion.Controllers
         }
 
         #region --教师职工生日信息
-        public ActionResult AddBriDay(Shengri model)
-        {
-            SQLHelper sqlh = new SQLHelper();
-            const string AddBriDaysql = @"INSERT INTO dbo.TB_ShengRi
-                                       ( BianHao,XingMing,ShengRi)
-                                        VALUES  ( @BianHao, 
-                                                  @XingMing,
-			                                      @ShengRi
-            ) ";
-            SqlParameter[] para = new SqlParameter[]
-            {
-              new SqlParameter("BianHao",model.BianHao),
-              new SqlParameter("XingMing", model.XingMing),
-              new SqlParameter("ShengRi", model.ShengRi)
-             };
-            sqlh.ExecData(AddBriDaysql, para);
-            return RedirectToAction("ScanBirthdayIndex", "EmployeeBenefits");
-        }
+        
 
 
         #endregion
